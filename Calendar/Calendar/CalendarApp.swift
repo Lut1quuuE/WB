61ee8adccc20ca492442efcc6e057700590f34dc
Ramazan <rnazaev@list.ru>
2024-07-15 21:29:43 +0500

    Calendar

---
+++ Calendar/Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Ramazan Nazaev on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
